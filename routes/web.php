d44490218b56bc3071dddac97b3f8da1e2b3d06a
takatani koki <tktn0305@gmail.com>
2020-02-29 12:33:57 +0900

    kadai14-3-6

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,20 @@ Route::get('/', function () {
     });
 Route::group(["prefix"=>"admin"],function(){
     Route::get("news/create","Admin\NewsController@add")->middleware('auth');
+    Route::post('news/create','Admin\NewsController@create'); #追記　PHP laravel 14
 });
-Route::get("admin/profile/create","Admin\ProfileController@add");
-Route::get("admin/profile/edit","Admin\ProfileController@editAction");
+Route::get("admin/profile/create","Admin\ProfileController@add")->middleware('auth');
+
+#追記　php laravel 14 課題
+Route::post("admin/profile/create","Admin\ProfileController@create");
+#追記　php laravel 14 終了
+
+Route::get("admin/profile/edit","Admin\ProfileController@editAction")->middleware('auth');
+
+#追記　php laravel 14 課題
+Route::post("admin/profile/edit","Admin\ProfileController@update");
+#追記　php laravel 14 終了
+
 
 Auth::routes();
 
