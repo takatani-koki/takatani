cab0fc609774b5a5e59d7b7f75f63efc96f4a7bd
takatani koki <tktn0305@gmail.com>
2020-04-19 18:28:24 +0900

    third commit

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -15,7 +15,6 @@
               <img src="{{ $headline->image_path }}">
               @endif
             </div>
-@csrf 
             <div class="title p-2">
               <h1>{{ str_limit($headline->title, 70) }}</h1>
             </div>
@@ -51,7 +50,6 @@
             <img src="{{ $post->image_path }}">
             @endif
           </div>
-@csrf
         </div>
       </div>
       <hr color="#c0c0c0">
